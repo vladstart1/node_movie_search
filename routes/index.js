622fc5898362a91b52175630b1b64e9f385b9a01
vladstart1 <vladstart1@hotmail.com>
2018-02-26 01:23:53 +0200

    Final commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const public = require('./public')
+const home = require('./home.js');
+const search = require('./search.js');
+const notFound = require('./notFound.js');
+
+module.exports = {
+	public,
+	home,
+	search,
+	notFound
+};
